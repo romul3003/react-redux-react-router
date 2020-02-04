5599169b724eae8384ba9d7ca8ab6ea22acf5a11
Roman Zakrevskyi <romul3003@gmail.com>
2020-02-04 19:41:34 +0200

    Add basic structure.

--- src/App.js
+++ src/App.js
@@ -3,11 +3,11 @@ import React, { Component } from 'react'
 class App extends Component {
 	render() {
 		return (
-			<>
+			<div className="app">
 				<header>Header</header>
 				<main>Main</main>
 				<footer>Footer</footer>
-			</>
+			</div>
 		)
 	}
 }
